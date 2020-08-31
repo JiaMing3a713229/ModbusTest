8d17a7060ef6fa4e49d5e57cf8b7690bcc210919
JiaMing3a713229 <ty10410205@gmail.com>
2020-08-31 22:05:13 +0800

    學習registers

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ try:
     client1 = ModbusTcpClient(host,port)
     connection = client1.connect()
     print ('connection to  '+host)
-    request1 = client1.write_register(0x01,97,unit=0x01)
+    #request1 = client1.write_register(0x01,97,unit=0x01)
+    #requeset1=client1.read_coils(0x02,64,unit=0x01)
+    #print(requeset1.bits[0])
+    request1 = client1.read_holding_registers(0x01,64,unit=0x01)
+    result1=request1.registers
+    print(result1)
+    print(result1[0])
 
 
 
